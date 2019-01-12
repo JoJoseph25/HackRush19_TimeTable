a3c9a4a8f9b11ec31b6594c0ade653ac24965e32
Joel-17510031 <35612321+Joel-17510031@users.noreply.github.com>
2019-01-13 02:27:02 +0530

    Useful Function 2
    Useful Function Commit 2
    4 Searching and returning desired Chromosome List

--- Useful_funct.py
+++ Useful_funct.py
@@ -14,3 +14,41 @@ def search(dict,value):
        return value
    else:
        return dict[list(dict.keys())[random.randint(0,len(dict)-1)]]
+
+# Search / Sort Chromosomes
+def partition(A, p, r, L, R):
+    pivot = int(A[r][L:R],2)
+    i = p-1
+    for j in range(p,r):
+        if int(A[j][L:R], 2) <= pivot:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i+1], A[r] = A[r], A[i+1]
+    return i+1
+def quicksort(A, p, r, L, R):
+    if p < r:
+        q = partition(A, p, r, L, R)
+        quicksort(A, p, q-1, L, R)
+        quicksort(A, q+1, r, L, R)
+
+B = ['01110', '11001', '01101', '01011', '11100', '11010', '00111', '10011','10101', '10110']
+
+import copy
+
+def search(arr, ptrn, L, R):
+    found = []
+    for s in arr:
+        if s[L:R] == ptrn:
+            found.append(s)
+    return found
+
+# print (search(B, '01', 0,2))
+
+# # def sort(arr, L, R):
+# temp = []
+# for i in range(len(B)):
+#     temp.append(B[i])
+# quicksort(temp,0, len(B)-1, 0, 2 )
+
+# print (B)
+# print (temp)
